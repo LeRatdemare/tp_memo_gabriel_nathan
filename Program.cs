ee2003ecb56e6b96170d8ff1c63861768648421e
Nathan Lufuluabo <lufuluabon@outlook.fr>
2022-11-23 00:35:01 +0100

    Début du code de genererNouvelleGrille.
    Implémentation de indexOfCharInTab

--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ int compteur = 0;
 
 void genererNouvelleGrille(int[,] grille)
 {
-    // A coder
+    // On commence par mettre des -1 de partout
+    remplirGrille2Dimensions(grille, -1);
+
+    /* Ensuite on va choisir un caractère au hasard sur une plage donnée
+    dont on va placer tous les éléments du n-uplet sur la grille. */
+    char[] dejaPlaces = new char[nbUplets];
+    Random gen = new Random();
+
+    for (int i = 0; i < nbUplets; i++)
+    {
+        // On va générer un caractère aléatoire tant qu'on en a un qui est dans dejaPlaces
+        char c;
+        do
+        {
+            c = (char)('a' + gen.Next(0, nbUplets));
+        } while (indexOfCharInTab(dejaPlaces, c) != -1);
+
+        // Ensuite on place toutes les cartes -c- dans la grille
+        // A coder
+    }
+}
+
+int indexOfCharInTab(char[] tab, char c)
+{
+    int i = 0;
+    while (i < tab.Length && tab[i] != c) i++;
+    if (i < tab.Length) return i; // Si on trouve l'élément on renvoie sa position
+    return -1; // -1 sinon
 }
 
 void remplirGrille2Dimensions(int[,] grille, int valeur)
