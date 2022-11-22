e566b5fa099ee483c3a825afe4a21a8faeb04c24
Nathan Lufuluabo <lufuluabon@outlook.fr>
2022-11-22 15:47:13 +0100

    Préparation des méthodes et des variables globales

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write($"Saisir le nombre de cartes par n-uplet : ");
+int nbCartesParUplet = int.Parse(Console.ReadLine()!);
+Console.Write($"Saisir le nombre de n-uplet(s) : ");
+int nbUplets = int.Parse(Console.ReadLine()!);
+int[,] tableauDesReponses = new int[nbCartesParUplet, nbUplets]; // A générer
+genererNouvelleGrille(tableauDesReponses);
+int[,] tableauJoueur = new int[nbCartesParUplet, nbUplets]; // Mettre des -1 partout
+char dernierCoupJoue; // A initialiser après le 1er coup
+int compteur = 0;
+
+void genererNouvelleGrille(int[,] grille)
+{
+    // A coder
+}
+
+void remplirGrille2Dimensions(int[,] grille, int valeur)
+{
+    // A coder
+}
+
+void jouerUnCoup(int ligne, int colonne)
+{
+    // A coder
+}
+
+void afficherGrille()
+{
+    // A coder
+}
+
+bool aGagne()
+{
+    return false; // A coder
+}
